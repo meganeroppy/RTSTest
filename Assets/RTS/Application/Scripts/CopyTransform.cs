5576df0db636d5a9403dcfb573bc9a8078812e6f
meganeroppy <roppy@hotmail.co.jp>
2017-11-02 20:01:20 +0900

    シーンを作成 現在角度だけ同期してる 頭は比較的まとも 手足の回転はなにかおかしい

---
+++ Assets/RTS/Application/Scripts/CopyTransform.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+/// <summary>
+/// CopyTransform.
+/// </summary>
+public class CopyTransform : MonoBehaviour
+{
+	public	GameObject	copySource	= null;
+    public bool useRotation = true;
+    public GameObject offsetObject;
+
+
+	/// <summary>
+	/// Update.
+	/// </summary>
+	void Update()
+	{
+		if( !copySource )
+		{
+			return;
+		}
+
+		if( gameObject.activeSelf != copySource.activeSelf )
+		{
+			gameObject.SetActive( copySource.activeSelf );
+		}
+
+        if (offsetObject)
+        {
+            transform.localPosition = copySource.transform.position + offsetObject.transform.position;
+        }
+        else
+        {
+            transform.localPosition = copySource.transform.position;
+        }
+        //非头部
+        if (useRotation)
+        {
+            transform.localRotation = copySource.transform.rotation;
+        }
+
+        else
+        {
+            transform.localPosition -= InputTracking.GetLocalPosition(VRNode.Head);
+        }
+
+    }
+}
