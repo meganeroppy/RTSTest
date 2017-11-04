aa1877019d0ee078cdb101e163c65ada786943d4
meganeroppy <roppy@hotmail.co.jp>
2017-11-04 15:27:19 +0900

    UnityOSCアセットを追加

---
+++ Assets/Script/LoadedConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadedConfig  {
+	public static LoadedConfig instance;
+
+	public LoadedConfig()
+	{
+		data = new List<KeyValuePair<string, string>>();
+	}
+	public string serverId;
+	public List<KeyValuePair<string, string>> data;
+}
