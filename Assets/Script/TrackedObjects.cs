bd3579c64953e75bf87ba3b61f3ed9baf08e87da
meganeroppy <roppy@hotmail.co.jp>
2017-11-22 11:02:20 +0900

    自分か相手の時で子オブジェクトを適宜有効無効化

--- Assets/Script/TrackedObjects.cs
+++ Assets/Script/TrackedObjects.cs
@@ -13,14 +13,37 @@ public class TrackedObjects : Photon.MonoBehaviour {
 	[SerializeField]
 	private CopyTransform leftHand;
 
+	[SerializeField]
+	private GameObject myCamera;
+
+	[SerializeField]
+	private GameObject headModel;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		// 自身でなければCopyTransformを削除
+		if( photonView != null)
+		{
+			if( !photonView.isMine  )
+			{
+				var children = GetComponentsInChildren<CopyTransform>();
+				for( int i = children.Length -1 ; i >= 0 ; --i )
+				{
+					Destroy( children[i] );
+				}
+
+			}
+
+			myCamera.SetActive( photonView.isMine );
+			headModel.SetActive( !photonView.isMine );
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
-	{
+	{		
 		// とりあえず仮でRを押して頭の回転リセット
 		if( Input.GetKeyDown(KeyCode.R) )
 		{
