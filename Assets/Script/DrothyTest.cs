0122a3937103a86ba785d28fc8db829f56d22645
meganeroppy <roppy@hotmail.co.jp>
2017-12-01 19:06:33 +0900

    ドロシーアニメ再生テスト

--- Assets/Script/DrothyTest.cs
+++ Assets/Script/DrothyTest.cs
@@ -9,16 +9,53 @@ public class DrothyTest : MonoBehaviour
 	 	
 	public Transform drothyBase;
 
+	enum Anim
+	{
+		Stand,
+		MouseOpen,
+		MouseSmile,
+		EyeClose,
+		Go2,
+		Furi2,
+		Count,
+	}
+
+	int animIdx = 0;
+
 	// Update is called once per frame
 	void Update () 
+	{
+		UpdatePosition();
+		UpdateInput();
+	}
+
+	void UpdatePosition()
 	{
 		var h = Input.GetAxis("Horizontal");
 		var v = Input.GetAxis("Vertical");
 
-		drothyBase.position += new Vector3( h, 0, v ) * speed * Time.deltaTime;
-	}
+		drothyBase.position += new Vector3( h, 0, v ) * speed * Time.deltaTime;	}
 
-	void ExecAnim(){}
+	void UpdateInput()
+	{
+		if( Input.GetKeyDown( KeyCode.P ) )
+		{
+			ExecAnim();
+		}
+	}
+	void ExecAnim()
+	{
+		animIdx++;
+
+		if( animIdx >= (int)Anim.Count )
+		{
+			animIdx = 0;
+		}
+
+		var newAnim = (Anim)animIdx;
+		anim.SetTrigger( newAnim.ToString() );
+		Debug.Log( newAnim.ToString() + " is triggered " );
+	}
 
 	void ChangeColor(){}
 }
