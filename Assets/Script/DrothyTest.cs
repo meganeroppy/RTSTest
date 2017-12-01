c955700a9aa86b1c21c8f92b01b92fe9d1545206
meganeroppy <roppy@hotmail.co.jp>
2017-12-01 18:38:03 +0900

    ドロシー制御更新

---
+++ Assets/Script/DrothyTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrothyTest : MonoBehaviour
+{
+	public  float speed = 5f;
+	public Animator anim;
+	 	
+	public Transform drothyBase;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		var h = Input.GetAxis("Horizontal");
+		var v = Input.GetAxis("Vertical");
+
+		drothyBase.position += new Vector3( h, 0, v ) * speed * Time.deltaTime;
+	}
+
+	void ExecAnim(){}
+
+	void ChangeColor(){}
+}
