f27594c6b49f9553b6b1f6a1fc3e98e7b6527716
meganeroppy <roppy@hotmail.co.jp>
2017-12-07 05:21:51 +0900

    上下運動する足シミュレータ（やっつけ）を作成

---
+++ Assets/SimulateWalk.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulateWalk : MonoBehaviour
+{
+	public Transform[] feet;
+
+	public float groundHeight = 0;
+	public float height = 1f;
+	public float speed = 0.1f;
+
+	private int idx = 0;
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine( Exec() );
+	}
+	
+	IEnumerator Exec()
+	{
+		var foot = feet[ idx ];
+
+		// ascend
+		while( foot.position.y < groundHeight + height )
+		{
+			foot.position += Vector3.up *  speed * Time.deltaTime;
+			yield return null;
+		}
+
+		// descend
+		while( foot.position.y > groundHeight  )
+		{
+			foot.position += -Vector3.up *  speed * Time.deltaTime;
+			yield return null;
+		}
+
+		idx = (idx+1) % feet.Length;
+
+		StartCoroutine( Exec() );
+	}
+
+}
