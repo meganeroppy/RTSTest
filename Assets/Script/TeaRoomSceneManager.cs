1ae48a6f0aca37c3274a62273a7c1f9a3ae435c9
meganeroppy <roppy@hotmail.co.jp>
2017-12-20 21:48:27 +0900

    ティールームシーンの制御クラスを作成

---
+++ Assets/Script/TeaRoomSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ティールームのシーン制御
+/// </summary>
+public class TeaRoomSceneManager : MonoBehaviour
+{
+    /// <summary>
+    /// プレイヤーを大きくなる演出（※プレイヤー以外の環境オブジェクトを小さくして表現する
+    /// </summary>
+    public void LargenPlayers()
+    {
+
+    }
+
+    /// <summary>
+    /// プレイヤーを小さくする演出（※プレイヤー以外の環境オブジェクトを大きくして表現する）
+    /// </summary>
+    public void SmallenPlayers()
+    {
+    }
+
+
+}
