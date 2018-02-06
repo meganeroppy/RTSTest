670ebfdf6075f7b01d1968cd2d5edbae79473246
meganeroppy <roppy@hotmail.co.jp>
2018-02-06 20:29:52 +0900

    参加型ナビゲータの挙動を修正

--- Assets/Scripts/ObserverController.cs
+++ Assets/Scripts/ObserverController.cs
@@ -21,10 +21,13 @@ public class ObserverController : NetworkBehaviour {
     [SerializeField]
     GameObject navigateShotPrefab;
 
+	private TrackedObjects trackedObjects;
+
     // Use this for initialization
     void Start () 
 	{
 		cameraRotate = transform.localRotation;
+		trackedObjects = GetComponent<TrackedObjects>();
 	}
 
     // Update is called once per frame
@@ -92,9 +95,13 @@ public class ObserverController : NetworkBehaviour {
         }			
     }		
 
+	bool pullingRightIndexTrigger = false;
+	bool pullingLeftIndexTrigger = false;
+	bool pullingRightHandTrigger = false;
+	bool pullingLeftHandTrigger = false;
+
 	/// <summary>
 	/// 入力管理
-	/// 場合によってはInputMode次第で制限
 	/// </summary>
     private void CheckInput()
     {
@@ -102,23 +109,73 @@ public class ObserverController : NetworkBehaviour {
         OVRInput.Update();
 
         // ナビゲートショット発射
-        if (
-			Input.GetButtonDown("Fire1") || // マウス左クリック
-			OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger) || // Touchの右人差し指トリガー
-			OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger)  // Touchの左人差し指トリガー
-		)
-        {
-            CmdFireNavigateShot();
-        }
+		{
+	        if (
+				Input.GetButtonDown("Fire1")// マウス左クリック
+			)
+	        {
+	            FireNavigateShotWithMouse();
+	        }
+
+			var pullRight = OVRInput.Get (OVRInput.RawAxis1D.RIndexTrigger) > 0;
+
+			if( pullRight && !pullingRightIndexTrigger )
+			{
+				pullingRightIndexTrigger = true;
+
+				// 右手から発射
+				FireNavigateShot(true);
+			}
+			else if( !pullRight )
+			{
+				pullingRightIndexTrigger = false;
+			}
+
+			var pullLeft = OVRInput.Get (OVRInput.RawAxis1D.LIndexTrigger) > 0;
+
+			if( pullLeft && !pullingLeftIndexTrigger )
+			{
+				pullingLeftIndexTrigger = true;
+
+				// 左手から発射
+				FireNavigateShot(false);
+			}
+			else if( !pullLeft )
+			{
+				pullingLeftIndexTrigger = false;
+			}
+		}
 
 		// イベントを進める
-		if(
-			Input.GetKeyDown(KeyCode.P) || // キーボードのP
-			OVRInput.GetDown(OVRInput.RawButton.RHandTrigger) || // Touchの右中指トリガー
-			OVRInput.GetDown(OVRInput.RawButton.LHandTrigger)  // Touchの左中指トリガー
-		)
 		{
-            CmdProceesSequence();
+			if( Input.GetKeyDown(KeyCode.P) )// キーボードのP
+			{
+				CmdProceesSequence();
+			}
+
+			var pullRight = OVRInput.Get (OVRInput.RawAxis1D.RHandTrigger) > 0;
+
+			if( pullRight & !pullingRightHandTrigger )
+			{
+				pullingRightHandTrigger = true;
+				CmdProceesSequence();
+			}
+			else if( !pullRight )
+			{
+				pullingRightHandTrigger = false;
+			}
+
+			var pullLeft = OVRInput.Get (OVRInput.RawAxis1D.LHandTrigger) > 0;
+
+			if( pullLeft & !pullingLeftHandTrigger )
+			{
+				pullingLeftHandTrigger = true;
+				CmdProceesSequence();
+			}
+			else if( !pullLeft )
+			{
+				pullingLeftHandTrigger = false;
+			}				
 		}
     }
 
@@ -128,25 +185,55 @@ public class ObserverController : NetworkBehaviour {
         var em = EventManager.instance;
         if (em == null) return;
         em.ProceedSequence();
-
     }
 
-    /// <summary>
-    /// 道筋やヒントをプレイヤーに与えるためのショットを発射
-    /// </summary>
+	/// <summary>
+	/// 道筋やヒントをプレイヤーに与えるためのショットを発射
+	/// キーボード操作版
+	/// </summary>
+	[Client]
+	private void FireNavigateShotWithMouse()
+	{
+		if( Camera.main == null ) return;
+		
+		var clickPosition = Input.mousePosition;
+		// Z軸修正
+		clickPosition.z = 10f;
+		var targetPos = Camera.main.ScreenToWorldPoint(clickPosition);
+
+		// 生成はサーバー側で行う
+		CmdFireNavigateShotWithMouse(targetPos);
+	}
+
+	/// <summary>
+	/// 道筋やヒントをプレイヤーに与えるためのショットを発射
+	/// Touch操作版
+	/// </summary>
+	[Client]
+	private void FireNavigateShot( bool right )
+	{
+		var obj = right ? trackedObjects.RightHandObject : trackedObjects.LeftHandObject;
+		// 生成はサーバー側で行う
+		CmdFireNavigateShot(obj.rotation);
+	}
+
     [Command]
-    private void CmdFireNavigateShot()
+	private void CmdFireNavigateShotWithMouse( Vector3 targetPos )
 	{
         var obj = Instantiate(navigateShotPrefab);
         obj.transform.position = transform.position;
-
-        var clickPosition = Input.mousePosition;
-        // Z軸修正
-        clickPosition.z = 10f;
-        var targetPos = Camera.main.ScreenToWorldPoint(clickPosition);
-
         obj.transform.LookAt(targetPos);
 
         NetworkServer.Spawn(obj);
+	}
+
+	[Command]
+	private void CmdFireNavigateShot( Quaternion rotation )
+	{
+		var obj = Instantiate(navigateShotPrefab);
+		obj.transform.position = transform.position;
+		obj.transform.rotation = rotation;
+
+		NetworkServer.Spawn(obj);
 	}
 }
