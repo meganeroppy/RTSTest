8fe4745b1438cfbc5f34fb48f10952b3e903817f
meganeroppy <roppy@hotmail.co.jp>
2018-02-22 22:53:50 +0900

    こどもアバター制御クラス作成

---
+++ Assets/Scripts/KidAvatarController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// こどもアバターの制御
+/// </summary>
+public class KidAvatarController : AvatarController
+{
+	private List<IKControl> kidsList;
+
+	private Transform objectRoot;
+
+	private void Start()
+	{
+		kidsList = new List<IKControl>();
+
+		for( int i = 0 ; i < transform.childCount ; i++ )
+		{
+			kidsList.Add( transform.GetChild( i ).GetComponent<IKControl>());
+		}
+
+		SetActiveKid( 0 );
+	}
+
+	public void SetActiveKid( int index )
+	{
+		// いったんすべて無効
+		foreach( IKControl k in kidsList )
+		{
+			k.gameObject.SetActive( false );
+		}
+
+		index %= kidsList.Count;
+
+		var kid = kidsList[ index ];
+
+		kid.gameObject.SetActive( true );
+
+		objectRoot = kid.transform;
+
+		var mRoot = kid.transform.Find("ROOT");
+		if( !mRoot ) 
+		{
+			Debug.LogError(" [ROOT] がみつからない");
+			return;
+		}
+
+		modelRoot = mRoot;
+	}
+
+	protected override void UpdatePositionAndRotation()
+	{
+		if( objectRoot == null ) return;
+
+		objectRoot.SetPositionAndRotation(ownerPosition, ownerRotation);
+	}
+}
